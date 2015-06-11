21349c43d9a7d0a5e25bf6faeaccae8107755ca1
Fedor Indutny <fedor@indutny.com>
2015-06-11 22:06:03 +0200

    decoder: use OffsetBuffer

--- test/decoder-test.js
+++ test/decoder-test.js
@@ -10,41 +10,44 @@ describe('hpack/decoder', function() {
 
   describe('integer', function() {
     it('should decode 10 in prefix-5 (C1.1)', function() {
-      assert.equal(decoder.decodeInt([ 0b11101010 ], 0, 5), 10);
+      decoder.push([ 0b11101010 ]);
+      assert.equal(decoder.decodeInt(5), 10);
     });
 
     it('should decode 1337 in prefix-5 (C1.2)', function() {
-      assert.equal(decoder.decodeInt([
-        0b11111111, 0b10011010, 0b00001010
-      ], 0, 5), 1337);
+      decoder.push([ 0b11111111, 0b10011010, 0b00001010 ]);
+      assert.equal(decoder.decodeInt(5), 1337);
     });
 
     it('should decode 42 at octect boundary (C1.3)', function() {
-      assert.equal(decoder.decodeInt([ 0b00101010 ], 0, 8), 42);
+      decoder.push([ 0b00101010 ]);
+      assert.equal(decoder.decodeInt(8), 42);
     });
 
     it('should throw on empty input', function() {
       assert.throws(function() {
-        decoder.decodeInt([ ], 0, 5);
+        decoder.decodeInt(5);
       });
     });
 
     it('should throw on incomplete int', function() {
+      decoder.push([ 0b11111111, 0b10011010 ]);
       assert.throws(function() {
-        decoder.decodeInt([ 0b11111111, 0b10011010 ], 0, 5);
+        decoder.decodeInt(5);
       });
     });
 
     it('should throw on overflowing int', function() {
+      decoder.push([
+        0b11111111,
+        0b10011010,
+        0b10011010,
+        0b10011010,
+        0b10011010,
+        0b10011010
+      ]);
       assert.throws(function() {
-        decoder.decodeInt([
-          0b11111111,
-          0b10011010,
-          0b10011010,
-          0b10011010,
-          0b10011010,
-          0b10011010
-        ], 0, 5);
+        decoder.decodeInt(5);
       });
     });
   });
