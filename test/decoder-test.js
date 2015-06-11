4da859aefda8fd745746323dc9d4da5d347db9f2
Fedor Indutny <fedor@indutny.com>
2015-06-11 22:36:11 +0200

    decoder: decode bits and strings

--- test/decoder-test.js
+++ test/decoder-test.js
@@ -8,32 +8,45 @@ describe('hpack/decoder', function() {
     decoder = hpack.decoder.create();
   });
 
+  describe('bit', function() {
+    it('should decode number bit-by-bit', function() {
+      decoder.push([ 0b11101010, 0b10101111 ]);
+      var out = '';
+      for (var i = 0; i < 16; i++)
+        out += decoder.decodeBit();
+      assert.equal(out, '11101010' + '10101111');
+    });
+  });
+
   describe('integer', function() {
-    it('should decode 10 in prefix-5 (C1.1)', function() {
+    it('should decode 10 in prefix-5 (C.1.1)', function() {
       decoder.push([ 0b11101010 ]);
-      assert.equal(decoder.decodeInt(5), 10);
+      decoder.skipBits(3);
+      assert.equal(decoder.decodeInt(), 10);
     });
 
-    it('should decode 1337 in prefix-5 (C1.2)', function() {
+    it('should decode 1337 in prefix-5 (C.1.2)', function() {
       decoder.push([ 0b11111111, 0b10011010, 0b00001010 ]);
-      assert.equal(decoder.decodeInt(5), 1337);
+      decoder.skipBits(3);
+      assert.equal(decoder.decodeInt(), 1337);
     });
 
-    it('should decode 42 at octect boundary (C1.3)', function() {
+    it('should decode 42 at octect boundary (C.1.3)', function() {
       decoder.push([ 0b00101010 ]);
       assert.equal(decoder.decodeInt(8), 42);
     });
 
     it('should throw on empty input', function() {
       assert.throws(function() {
-        decoder.decodeInt(5);
+        decoder.decodeInt();
       });
     });
 
     it('should throw on incomplete int', function() {
       decoder.push([ 0b11111111, 0b10011010 ]);
+      decoder.skipBits(3);
       assert.throws(function() {
-        decoder.decodeInt(5);
+        decoder.decodeInt();
       });
     });
 
@@ -46,9 +59,19 @@ describe('hpack/decoder', function() {
         0b10011010,
         0b10011010
       ]);
+      decoder.skipBits(3);
       assert.throws(function() {
-        decoder.decodeInt(5);
+        decoder.decodeInt();
       });
+    });
+  });
+
+  describe('string', function() {
+    it('should decode literal from (C.2.1)', function() {
+      decoder.push([ 0x0a ]);
+      decoder.push(new Buffer('custom-key'));
+
+      assert.equal(decoder.decodeStr().toString(), 'custom-key');
     });
   });
 });
